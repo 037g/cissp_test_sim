869869104338e344dd7cc33d70be63ace6c8f5de
MJ <gatewaycd@live.com>
2020-06-06 17:42:27 -0600

    Converted questions to json format and updated main.py to compensate

--- main.py
+++ main.py
@@ -5,26 +5,26 @@
 from questengine import QuestionEngine
 from helpers import bc, ask, clear
 
-#Main
+# Main
 if __name__ == "__main__":
     qe = QuestionEngine()
     qc = 0
-	
+
     clear()
     while True:
         qc = input("How many questions do you want to practice? 1-{} (X to exit): \n"
-		     .format(qe.totalQuestions))
+                   .format(qe.totalQuestions))
         if qc == "x" or qc == "X":
             exit()
-        elif qc.isdigit() and (int(qc) > 0 
-		                  and  int(qc) <= qe.totalQuestions):
+        elif qc.isdigit() and (int(qc) > 0
+                               and int(qc) <= qe.totalQuestions):
             break
         else:
             clear()
-			
-    i       = 0
+
+    i = 0
     is_done = False
-    result  = None
+    result = None
     while True:
         try:
             qn = i + 1
@@ -35,32 +35,31 @@ if __name__ == "__main__":
             if result == 'correct':
                 qe.correct += 1
                 i += 1
-                print("\n{}Correct!!!\n\n{}".format(bc.OKGREEN, bc.ENDC))
+                print("\n{}Correct!!! The Answer is: {}\n\n{}".format(bc.OKGREEN, qe.getSolutionText(i - 1), bc.ENDC))
             elif result == 'wrong':
                 qe.incorrect += 1
                 i += 1
                 print("\n{} Wrong!!! The correct Answer is: {}{}\n\n{}".format(bc.WARNING,
-                        bc.OKGREEN, qe.getSolutionText(i), bc.ENDC))
+                                                                               bc.OKGREEN, qe.getSolutionText(i - 1), bc.ENDC))
             elif result == "exit":
                 qn = qc
 
             if int(qn) == int(qc):
-                break
+                raise Exception()
             else:
                 input("Press ENTER to continue...")
-		
+
         except Exception as e:
-            clear()
             if int(qn) == int(qc):
+                input("Press ENTER to view results...")
                 break
             else:
                 pass
 
-
     clear()
-    answered = qe.correct + qe.incorrect 
-    right   = qe.correct
-    wrong   = qe.incorrect
+    answered = qe.correct + qe.incorrect
+    right = qe.correct
+    wrong = qe.incorrect
     if answered == 0:
         righta = "0.0"
         wronga = "0.0"
@@ -69,15 +68,19 @@ if __name__ == "__main__":
         wronga = wrong / int(answered) * 100
     rightq = int(right) / int(qc) * 100
     wrongq = wrong / int(qc) * 100
-    skipq  = int(qc) - int(answered)
+    skipq = int(qc) - int(answered)
     print("Results:")
     print("Questions answered: {} of {}\n".format(answered, qc))
 
     print("Scores from questions answered:")
-    print("{}# Right: {} {}% {}".format(bc.OKGREEN, str(right), righta, bc.ENDC))
-    print("{}# Wrong: {} {}% \n{}".format(bc.WARNING, str(wrong), wronga, bc.ENDC))
+    print("{}# Right: {} {}% {}".format(
+        bc.OKGREEN, str(right), righta, bc.ENDC))
+    print("{}# Wrong: {} {}% \n{}".format(
+        bc.WARNING, str(wrong), wronga, bc.ENDC))
 
     print("Scores from total questions")
-    print("{}# Right: {} {}% {}".format(bc.OKGREEN, str(right), rightq, bc.ENDC))
-    print("{}# Wrong: {} {}% {}".format(bc.OKGREEN, str(wrong), wrongq, bc.ENDC))
-    print("{}# Skipped: {}\n{}".format(bc.WARNING, skipq, bc.ENDC)) 
+    print("{}# Right: {} {}% {}".format(
+        bc.OKGREEN, str(right), rightq, bc.ENDC))
+    print("{}# Wrong: {} {}% {}".format(
+        bc.WARNING, str(wrong), wrongq, bc.ENDC))
+    print("{}# Skipped: {}\n{}".format(bc.WARNING, skipq, bc.ENDC))
--- questengine.py
+++ questengine.py
@@ -1,113 +1,104 @@
 import json
 import random
 
+
 class QuestionEngine:
-	
-	def __init__(self, jsonfile:str='bank\cissp_questions.json',
-					   questionisrandom=True,
-					   answerisalpha:bool=True,
-					   answerisrandom:bool=True):
-		"""Use JSON files to make exams"""
-			
-		self._jsonFile         = jsonfile
-		self._questionIsRandom = questionisrandom
-		self._answerIsAlpha    = answerisalpha
-		self._answerIsRandom   = answerisrandom
 
-		self.correct           = 0
-		self.incorrect         = 0
-		
-		self.jsonData         = self.__loadJsonFile()
-		self.totalQuestions   = len(self.jsonData['questions'])
-		self.questionSet      = self.__complieQuestions()
+    def __init__(self, jsonfile: str = 'bank\cissp_questions.json',
+                 questionisrandom=True,
+                 answerisalpha: bool = True,
+                 answerisrandom: bool = True):
+        """Use JSON files to make exams"""
 
-		
-	def __loadJsonFile(self) -> dict:
-		"""Load the json question file"""
-		
-		with open(self._jsonFile) as f:
-			self.jsonData = json.load(f)
-			
-		return self.jsonData 
-	
-	
-	def __complieQuestions(self) -> dict:
-		"""Create dictionary of questions and question number"""
-		
-		if self._questionIsRandom:
-			questions = random.sample(range(0, self.totalQuestions), 
-									  self.totalQuestions)
-		else:
-			questions = list(range(0, self.totalQuestions))
-			
-		questionSet    = {}
-		currentAnswers = {}
-		for itr, question in enumerate(questions):
-			answers         = self.jsonData['questions'][question]['answers']
-			questionSection = self.jsonData['questions'][question]
-			answerKeys = '123456789'
-			if self._answerIsAlpha:
-				answerKeys = 'abcdefghi'
+        self._jsonFile = jsonfile
+        self._questionIsRandom = questionisrandom
+        self._answerIsAlpha = answerisalpha
+        self._answerIsRandom = answerisrandom
+        self._jsonData = self.__loadJsonFile()
 
-			answerValues = list(answers.keys())
-			if self._answerIsRandom:
-				random.shuffle(answerValues)
-		
-			currentAnswers = {}
-			for answer in range(len(answerKeys)):
-				if answer >= len(answerValues): 
-					break
-				else:
-					currentAnswers.update( { answerKeys[answer]:{
-										    answerValues[answer]:answers[answerValues[answer]] } } )
-			
-				questionSet[itr] = ( { 'question':    questionSection['question'],
-								       'answers':      currentAnswers, 
-								       'solution':    questionSection['solution'],
-									   'explanation': questionSection['explanation'] } )
+        self.correct = 0
+        self.incorrect = 0
+        self.totalQuestions = len(self._jsonData['questions'])
+        self.questionSet = self.__complieQuestions()
 
-		return questionSet
-	
-	
-	def getQuestion(self, questionnumber:int) -> str:
-		"""Return question from compiled questions"""
-		
-		return self.questionSet[questionnumber]['question']
-		
-	
-	def getAnswers(self, questionnumber:int) -> dict:
-		"""Return dictionary with answers for given question"""
-		
-		return self.questionSet[questionnumber]['answers']
-		
-		
-	def getSolutionText(self, questionnumber:int) -> str:
-		"""Return solution for given question"""
-		
-		solution     = self.questionSet[questionnumber]['solution']
-		answers      = self.questionSet[questionnumber]['answers']
-		solutiontext = ""
-		for key, value in answers.items():
-			for k, v in value.items():
-				if solution == k:
-					solutiontext = v
-			
-		return solutiontext
-		
-		
-	def getExplanation(self, questionnumber:int) -> str:
-		"""Return solution for given question"""
-		
-		return self.questionSet[questionnumer]['explanation']
-		
-		
-	def compareSolution(self, questionnumber:int, answerguess:int) -> bool:
-		"""Compare value to solution"""
-		
-		if answerguess == self.questionSet[questionnumber]['solution']:
-			return True
-		else:
-			return False
-		
-			
-	
+    def __loadJsonFile(self) -> dict:
+        """Load the json question file"""
+
+        with open(self._jsonFile) as f:
+            self._jsonData = json.load(f)
+        f.close()
+
+        return self._jsonData
+
+    def __complieQuestions(self) -> dict:
+        """Create dictionary of questions and question number"""
+
+        if self._questionIsRandom:
+            questions = random.sample(range(0, self.totalQuestions),
+                                      self.totalQuestions)
+        else:
+            questions = list(range(0, self.totalQuestions))
+
+        questionSet = {}
+        currentAnswers = {}
+        for itr, question in enumerate(questions):
+            answers = self._jsonData['questions'][question]['answers']
+            questionSection = self._jsonData['questions'][question]
+            answerKeys = '123456789'
+            if self._answerIsAlpha:
+                answerKeys = 'abcdefghi'
+
+            answerValues = list(answers.keys())
+            if self._answerIsRandom:
+                random.shuffle(answerValues)
+
+            currentAnswers = {}
+            for answer in range(len(answerKeys)):
+                if answer >= len(answerValues):
+                    break
+                else:
+                    currentAnswers.update({answerKeys[answer]: {
+                        answerValues[answer]: answers[answerValues[answer]]}})
+
+                questionSet[itr] = ({'question':    questionSection['question'],
+                                     'answers':      currentAnswers,
+                                     'solution':    questionSection['solution'],
+                                     'explanation': questionSection['explanation']})
+
+        return questionSet
+
+    def getQuestion(self, questionnumber: int) -> str:
+        """Return question from compiled questions"""
+
+        return self.questionSet[questionnumber]['question']
+
+    def getAnswers(self, questionnumber: int) -> dict:
+        """Return dictionary with answers for given question"""
+
+        return self.questionSet[questionnumber]['answers']
+
+    def getExplanation(self, questionnumber: int) -> str:
+        """Return solution for given question"""
+
+        return self.questionSet[questionnumber]['explanation']
+
+    def getSolutionText(self, questionnumber: int) -> str:
+        """Return solution for given question"""
+
+        solution = self.questionSet[questionnumber]['solution']
+        answers = self.questionSet[questionnumber]['answers']
+        solutiontext = ""
+        for key, value in answers.items():
+            for k, v in value.items():
+                if solution == k:
+                    solutiontext = v
+
+        return solutiontext
+
+    def compareSolution(self, questionnumber: int, answerguess: int) -> bool:
+        """Compare value to solution"""
+
+        if answerguess == self.questionSet[questionnumber]['solution']:
+            return True
+        else:
+            return False
